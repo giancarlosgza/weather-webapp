2d68a452f583862246e20fcb93af249e4fad7bfd
Giancarlos Garza <giancarlosgza@gmail.com>
2021-06-07 18:59:14 -0500

    SCSS added

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import VueMeta from 'vue-meta'
+
+import 'popper.js'
+import 'bootstrap'
 
 Vue.config.productionTip = false
+
+Vue.use(VueMeta);
 
 new Vue({
   router,
