99c692617bd6c18ea23486783f7eed822032bb75
Giancarlos Garza <giancarlosgza@gmail.com>
2021-06-10 15:09:34 -0500

    Api fetch working

---
+++ src/components/weather/IconWeather.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div v-if="weather.daily[number].weather[0].main === 'Clear'" class="mb-3">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
+        <path fill="none" d="M0 0h24v24H0z"/>
+        <path
+          d="M12 18a6 6 0 1 1 0-12 6 6 0 0 1 0 12zm0-2a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM11 1h2v3h-2V1zm0 19h2v3h-2v-3zM3.515 4.929l1.414-1.414L7.05 5.636 5.636 7.05 3.515 4.93zM16.95 18.364l1.414-1.414 2.121 2.121-1.414 1.414-2.121-2.121zm2.121-14.85l1.414 1.415-2.121 2.121-1.414-1.414 2.121-2.121zM5.636 16.95l1.414 1.414-2.121 2.121-1.414-1.414 2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z"
+          fill="rgba(255,173,0,1)"/>
+      </svg>
+    </div>
+    <div v-if="weather.daily[number].weather[0].main === 'Rain'" class="mb-3">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
+        <path fill="none" d="M0 0h24v24H0z"/>
+        <path
+          d="M16 18v-2h1a4 4 0 1 0-2.157-7.37A6 6 0 1 0 8 15.917v2.022A8.001 8.001 0 0 1 9 2a7.998 7.998 0 0 1 6.98 4.087A6 6 0 1 1 17 18h-1zm-5.768.732L12 16.964l1.768 1.768a2.5 2.5 0 1 1-3.536 0z"
+          fill="rgba(149,164,166,1)"/>
+      </svg>
+    </div>
+    <div v-if="weather.daily[number].weather[0].main === 'Clouds'" class="mb-3">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
+        <path fill="none" d="M0 0h24v24H0z"/>
+        <path
+          d="M9.5 6a6.5 6.5 0 0 0 0 13h7a4.5 4.5 0 1 0-.957-8.898A6.502 6.502 0 0 0 9.5 6zm7 15h-7a8.5 8.5 0 1 1 7.215-12.997A6.5 6.5 0 0 1 16.5 21z"
+          fill="rgba(149,164,166,1)"/>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IconWeather',
+  props: [
+    'weather',
+    'number'
+  ]
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,140 @@
 <template>
   <div class="home">
-    <Navbar></Navbar>
 
-    <div class="container">
-      <div class="row justify-content-center">
-        <div class="col-md-6">
+    <div class="container mb-5">
+      <div class="row justify-content-center my-3">
+        <div class="col-md-8">
           <div class="card">
             <div class="card-body">
-              <div>
-                <label for="input-city" class="mb-2">Search a city</label>
-                <input id="input-city" type="text" class="form-control" placeholder="New York, Dubai, Madrid, CDMX">
+              <div class="input-group">
+                  <span class="input-group-text input-group-text-transparent" id="input-city">
+                    <i class="material-icons-outlined">location_on</i>
+                  </span>
+                <input type="text" class="form-control form-control-border-bottom"
+                       placeholder="New York, Dubai, Madrid, CDMX"
+                       aria-label="New York, Dubai, Madrid, CDMX"
+                       aria-describedby="input-city" v-model="query">
               </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div class="row">
-        <div class="col-md-12">
-          <h6>Icon</h6>
-          <h6>Weather</h6>
-          <h6>City name</h6>
+      <div class="row justify-content-center" v-if="weather">
+        <div class="col-md-8 mb-3">
+          <div class="card">
+            <div class="card-body text-center">
+              <div class="mb-4">
+                <IconCurrentWeather :weather="weather" number="0"></IconCurrentWeather>
+                <h2 class="fw-500">{{ weather.current.temp }} °C</h2>
+                <h6 class="subtitle-1 text-capitalize">{{ weather.current.weather[0].description }}</h6>
+                <h6 class="subtitle-1 text-muted">{{ weather.timezone }}</h6>
+              </div>
+              <div>
+                <div class="row">
+                  <div class="col-4 col-md-4">
+                    <h6 class="subtitle-2 text-muted">
+                      <i class="material-icons-outlined mdc-18 me-1">thermostat</i>
+                      Feels like
+                    </h6>
+                    <h6 class="subtitle-1 fw-bold">{{ weather.current.feels_like }} °C</h6>
+                  </div>
+                  <div class="col-4 col-md-4">
+                    <h6 class="subtitle-2 text-muted">
+                      <i class="material-icons-outlined mdc-18 me-1">water_drop</i>
+                      Humidity
+                    </h6>
+                    <h6 class="subtitle-1 fw-bold">{{ weather.current.humidity }} %</h6>
+                  </div>
+                  <div class="col-4 col-md-4">
+                    <h6 class="subtitle-2 text-muted">
+                      <i class="material-icons-outlined mdc-18 me-1">air</i>
+                      Wind
+                    </h6>
+                    <h6 class="subtitle-1 fw-bold">{{ weather.current.wind_speed }} m/s</h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="scrolling-wrapper">
+          <div>
+            <div class="card">
+              <div class="card-body">
+                <div class="text-center">
+                  <IconWeather :weather="weather" number="0"></IconWeather>
+                  <h6 class="subtitle-2 text-muted text-capitalize">{{ weather.daily[0].weather[0].description }}</h6>
+                  <h6 class="subtitle-1 fw-500">Tomorrow</h6>
+                  <div class="text-center">
+                    <h6 class="subtitle-1">{{ weather.daily[0].temp.max }} - <span
+                      class="text-muted">{{ weather.daily[0].temp.min }}</span></h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="card">
+              <div class="card-body">
+                <div class="text-center">
+                  <IconWeather :weather="weather" number="1"></IconWeather>
+                  <h6 class="subtitle-2 text-muted text-capitalize">{{ weather.daily[1].weather[0].description }}</h6>
+                  <h6 class="subtitle-1 fw-500">Friday</h6>
+                  <div class="text-center">
+                    <h6 class="subtitle-1">{{ weather.daily[1].temp.max }} - <span
+                      class="text-muted">{{ weather.daily[1].temp.min }}</span></h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="card">
+              <div class="card-body">
+                <div class="text-center">
+                  <IconWeather :weather="weather" number="2"></IconWeather>
+                  <h6 class="subtitle-2 text-muted text-capitalize">{{ weather.daily[2].weather[0].description }}</h6>
+                  <h6 class="subtitle-1 fw-500">Saturday</h6>
+                  <div class="text-center">
+                    <h6 class="subtitle-1">{{ weather.daily[2].temp.max }} - <span
+                      class="text-muted">{{ weather.daily[2].temp.min }}</span></h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="card">
+              <div class="card-body">
+                <div class="text-center">
+                  <IconWeather :weather="weather" number="3"></IconWeather>
+                  <h6 class="subtitle-2 text-muted text-capitalize">{{ weather.daily[3].weather[0].description }}</h6>
+                  <h6 class="subtitle-1 fw-500">Sunday</h6>
+                  <div class="text-center">
+                    <h6 class="subtitle-1">{{ weather.daily[3].temp.max }} - <span
+                      class="text-muted">{{ weather.daily[3].temp.min }}</span></h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="card">
+              <div class="card-body">
+                <div class="text-center">
+                  <IconWeather :weather="weather" number="4"></IconWeather>
+                  <h6 class="subtitle-2 text-muted text-capitalize">{{ weather.daily[4].weather[0].description }}</h6>
+                  <h6 class="subtitle-1 fw-500">Monday</h6>
+                  <div class="text-center">
+                    <h6 class="subtitle-1">{{ weather.daily[4].temp.max }} - <span
+                      class="text-muted">{{ weather.daily[4].temp.min }}</span></h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
 
@@ -31,14 +145,39 @@
 
 <script>
 // @ is an alias to /src
-import Navbar from "@/components/includes/Navbar";
 import Footer from "@/components/includes/Footer";
+import IconWeather from "@/components/weather/IconWeather";
+import IconCurrentWeather from "@/components/weather/IconCurrentWeather";
 
 export default {
   name: 'Home',
   components: {
-    Footer,
-    Navbar
+    IconCurrentWeather,
+    IconWeather,
+    Footer
+  },
+  data() {
+    return {
+      api_key: process.env.VUE_APP_API_KEY,
+      url_base: process.env.VUE_APP_API_URL_BASE,
+      url_settings: process.env.VUE_APP_API_URL_SETTINGS,
+      query: 'lat=25.6667&lon=-100.3167',
+      weather: {}
+    }
+  },
+  mounted() {
+    this.getWeather()
+  },
+  methods: {
+    getWeather() {
+      fetch(`${this.url_base}?${this.query}&${this.url_settings}&appid=${this.api_key}`)
+        .then(res => {
+          return res.json()
+        }).then(this.setResults)
+    },
+    setResults(results) {
+      this.weather = results
+    }
   }
 }
 </script>
