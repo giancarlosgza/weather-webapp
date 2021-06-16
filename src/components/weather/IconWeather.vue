0bf9b028accb822240ec76f1c448b034f36a6d2f
Giancarlos Garza <giancarlosgza@gmail.com>
2021-06-16 13:28:18 -0500

    Fluent icon added

--- src/components/weather/IconWeather.vue
+++ src/components/weather/IconWeather.vue
@@ -1,59 +1,34 @@
 <template>
   <div>
+
     <!-- CLEAR -->
     <div v-if="weather.daily[number].weather[0].main === 'Clear'" class="mb-3">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
-        <path fill="none" d="M0 0h24v24H0z"/>
-        <path
-          d="M12 18a6 6 0 1 1 0-12 6 6 0 0 1 0 12zm0-2a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM11 1h2v3h-2V1zm0 19h2v3h-2v-3zM3.515 4.929l1.414-1.414L7.05 5.636 5.636 7.05 3.515 4.93zM16.95 18.364l1.414-1.414 2.121 2.121-1.414 1.414-2.121-2.121zm2.121-14.85l1.414 1.415-2.121 2.121-1.414-1.414 2.121-2.121zM5.636 16.95l1.414 1.414-2.121 2.121-1.414-1.414 2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z"
-          fill="rgba(255,173,0,1)"/>
-      </svg>
+      <img src="@/assets/img/weather-icons/icon-sun.png" class="img-fluid" width="48" height="48" alt="Sun Icon">
     </div>
     <!-- RAIN -->
     <div v-if="weather.daily[number].weather[0].main === 'Rain'" class="mb-3">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
-        <path fill="none" d="M0 0h24v24H0z"/>
-        <path
-          d="M16 18v-2h1a4 4 0 1 0-2.157-7.37A6 6 0 1 0 8 15.917v2.022A8.001 8.001 0 0 1 9 2a7.998 7.998 0 0 1 6.98 4.087A6 6 0 1 1 17 18h-1zm-5.768.732L12 16.964l1.768 1.768a2.5 2.5 0 1 1-3.536 0z"
-          fill="rgba(149,164,166,1)"/>
-      </svg>
+      <img src="@/assets/img/weather-icons/icon-rain.png" class="img-fluid" width="48" height="48" alt="Rain Icon">
     </div>
     <!-- CLOUDS -->
     <div v-if="weather.daily[number].weather[0].main === 'Clouds'" class="mb-3">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
-        <path fill="none" d="M0 0h24v24H0z"/>
-        <path
-          d="M9.5 6a6.5 6.5 0 0 0 0 13h7a4.5 4.5 0 1 0-.957-8.898A6.502 6.502 0 0 0 9.5 6zm7 15h-7a8.5 8.5 0 1 1 7.215-12.997A6.5 6.5 0 0 1 16.5 21z"
-          fill="rgba(149,164,166,1)"/>
-      </svg>
+      <img src="@/assets/img/weather-icons/icon-clouds.png" class="img-fluid" width="48" height="48" alt="Clouds Icon">
     </div>
     <!-- THUNDERSTORM -->
     <div v-if="weather.daily[number].weather[0].main === 'Thunderstorm'" class="mb-3">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
-        <path fill="none" d="M0 0h24v24H0z"/>
-        <path
-          d="M17 18v-2h.5a3.5 3.5 0 1 0-2.5-5.95V10a6 6 0 1 0-8 5.659v2.089a8 8 0 1 1 9.458-10.65A5.5 5.5 0 1 1 17.5 18l-.5.001zm-4-1.995h3l-5 6.5v-4.5H8l5-6.505v4.505z"
-          fill="rgba(52,72,94,1)"/>
-      </svg>
+      <img src="@/assets/img/weather-icons/icon-thunderstorm.png" class="img-fluid" width="48" height="48"
+           alt="Thunderstorm Icon">
     </div>
     <!-- DRIZZLE -->
     <div v-if="weather.daily[number].weather[0].main === 'Drizzle'" class="mb-3">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
-        <path fill="none" d="M0 0h24v24H0z"/>
-        <path
-          d="M17 18v-2h.5a3.5 3.5 0 1 0-2.5-5.95V10a6 6 0 1 0-8 5.659v2.089a8 8 0 1 1 9.458-10.65A5.5 5.5 0 1 1 17.5 18l-.5.001zm-8-2h2v4H9v-4zm4 3h2v4h-2v-4z"
-          fill="rgba(149,164,166,1)"/>
-      </svg>
+      <img src="@/assets/img/weather-icons/icon-light-rain.png" class="img-fluid" width="48" height="48"
+           alt="Drizzle Icon">
     </div>
     <!-- SNOW -->
     <div v-if="weather.daily[number].weather[0].main === 'Snow'" class="mb-3">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
-        <path fill="none" d="M0 0h24v24H0z"/>
-        <path
-          d="M13 16.268l1.964-1.134 1 1.732L14 18l1.964 1.134-1 1.732L13 19.732V22h-2v-2.268l-1.964 1.134-1-1.732L10 18l-1.964-1.134 1-1.732L11 16.268V14h2v2.268zM17 18v-2h.5a3.5 3.5 0 1 0-2.5-5.95V10a6 6 0 1 0-8 5.659v2.089a8 8 0 1 1 9.458-10.65A5.5 5.5 0 1 1 17.5 18l-.5.001z"
-          fill="rgba(195,195,195,1)"/>
-      </svg>
+      <img src="@/assets/img/weather-icons/icon-snow.png" class="img-fluid" width="48" height="48"
+           alt="Snow Icon">
     </div>
+
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -64,23 +64,14 @@
               <div class="d-flex justify-content-around">
 
                 <div class="text-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
-                    <path fill="none" d="M0 0h24v24H0z"/>
-                    <path
-                      d="M12 18a6 6 0 1 1 0-12 6 6 0 0 1 0 12zm0-2a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM11 1h2v3h-2V1zm0 19h2v3h-2v-3zM3.515 4.929l1.414-1.414L7.05 5.636 5.636 7.05 3.515 4.93zM16.95 18.364l1.414-1.414 2.121 2.121-1.414 1.414-2.121-2.121zm2.121-14.85l1.414 1.415-2.121 2.121-1.414-1.414 2.121-2.121zM5.636 16.95l1.414 1.414-2.121 2.121-1.414-1.414 2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z"
-                      fill="rgba(255,173,0,1)"/>
-                  </svg>
+                  <img src="@/assets/img/weather-icons/icon-sunrise.png" class="img-fluid" width="48" height="48"
+                       alt="Sunrise Icon">
                   <h6 class="subtitle-2 text-muted mt-2">Sunrise</h6>
                   <h6 class="subtitle-2">{{ weather.current.sunrise | moment("h:mm") }} am</h6>
                 </div>
 
                 <div class="text-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36">
-                    <path fill="none" d="M0 0h24v24H0z"/>
-                    <path
-                      d="M6.083 13a6 6 0 1 1 11.834 0h-2.043a4 4 0 1 0-7.748 0H6.083zM2 15h10v2H2v-2zm12 0h8v2h-8v-2zm2 4h4v2h-4v-2zM4 19h10v2H4v-2zm7-18h2v3h-2V1zM3.515 4.929l1.414-1.414L7.05 5.636 5.636 7.05 3.515 4.93zM19.07 3.515l1.414 1.414-2.121 2.121-1.414-1.414 2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z"
-                      fill="rgba(234,119,11,1)"/>
-                  </svg>
+                  <img src="@/assets/img/weather-icons/icon-sunset.png" class="img-fluid" width="48" height="48" alt="Sunset Icon">
                   <h6 class="subtitle-2 text-muted mt-2">Sunset</h6>
                   <h6 class="subtitle-2 mt-2">{{ weather.current.sunset | moment("h:mm") }} pm</h6>
                 </div>
