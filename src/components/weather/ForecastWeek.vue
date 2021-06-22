f0e65392a641edf32ca97e13bfe104b6f400916f
Giancarlos Garza <giancarlosgza@gmail.com>
2021-06-22 16:56:55 -0500

    Forecast days fix and moved to component

---
+++ src/components/weather/ForecastWeek.vue
@@ -0,0 +1,137 @@
+<template>
+  <div>
+    <div class="row" v-if="weather">
+      <div class="col-md-12 col-lg-8">
+        <h6 class="subtitle-1 fw-500 text-white">This week</h6>
+      </div>
+      <div class="scrolling-wrapper">
+        <!-- TOMORROW - 1 DAY -->
+        <div class="wrapper">
+          <div class="card bg-frosted">
+            <div class="card-body">
+              <div class="text-center">
+                <IconWeather :weather="weather" number="0"></IconWeather>
+                <h6 class="subtitle-2 text-muted text-capitalize">{{ weather.daily[0].weather[0].description }}</h6>
+                <h6 class="subtitle-1 fw-500">{{ weather.daily[0].dt | moment("dddd") }}</h6>
+                <div class="text-center">
+                  <h6 class="subtitle-1">{{ Math.round(weather.daily[0].temp.max) }}° - <span
+                    class="text-muted">{{ Math.round(weather.daily[0].temp.min) }}°</span></h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- 2 DAYS -->
+        <div class="wrapper">
+          <div class="card bg-frosted">
+            <div class="card-body">
+              <div class="text-center">
+                <IconWeather :weather="weather" number="1"></IconWeather>
+                <h6 class="subtitle-2 text-muted text-capitalize">{{ weather.daily[1].weather[0].description }}</h6>
+                <h6 class="subtitle-1 fw-500">{{ weather.daily[1].dt | moment("dddd") }}</h6>
+                <div class="text-center">
+                  <h6 class="subtitle-1">{{ Math.round(weather.daily[1].temp.max) }}° - <span
+                    class="text-muted">{{ Math.round(weather.daily[1].temp.min) }}°</span></h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- 3 DAYS -->
+        <div class="wrapper">
+          <div class="card bg-frosted">
+            <div class="card-body">
+              <div class="text-center">
+                <IconWeather :weather="weather" number="2"></IconWeather>
+                <h6 class="subtitle-2 text-muted text-capitalize">{{ weather.daily[2].weather[0].description }}</h6>
+                <h6 class="subtitle-1 fw-500">{{ weather.daily[2].dt | moment("dddd") }}</h6>
+                <div class="text-center">
+                  <h6 class="subtitle-1">{{ Math.round(weather.daily[2].temp.max) }}° - <span
+                    class="text-muted">{{ Math.round(weather.daily[2].temp.min) }}°</span></h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- 4 DAYS -->
+        <div class="wrapper">
+          <div class="card bg-frosted">
+            <div class="card-body">
+              <div class="text-center">
+                <IconWeather :weather="weather" number="3"></IconWeather>
+                <h6 class="subtitle-2 text-muted text-capitalize">{{ weather.daily[3].weather[0].description }}</h6>
+                <h6 class="subtitle-1 fw-500">{{ weather.daily[3].dt | moment("dddd") }}</h6>
+                <div class="text-center">
+                  <h6 class="subtitle-1">{{ Math.round(weather.daily[3].temp.max) }}° - <span
+                    class="text-muted">{{ Math.round(weather.daily[3].temp.min) }}°</span></h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- 5 DAYS -->
+        <div class="wrapper">
+          <div class="card bg-frosted">
+            <div class="card-body">
+              <div class="text-center">
+                <IconWeather :weather="weather" number="4"></IconWeather>
+                <h6 class="subtitle-2 text-muted text-capitalize">{{ weather.daily[4].weather[0].description }}</h6>
+                <h6 class="subtitle-1 fw-500">{{ weather.daily[4].dt | moment("dddd") }}</h6>
+                <div class="text-center">
+                  <h6 class="subtitle-1">{{ Math.round(weather.daily[4].temp.max) }}° - <span
+                    class="text-muted">{{ Math.round(weather.daily[4].temp.min) }}°</span></h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- 6 DAYS -->
+        <div class="wrapper">
+          <div class="card bg-frosted">
+            <div class="card-body">
+              <div class="text-center">
+                <IconWeather :weather="weather" number="5"></IconWeather>
+                <h6 class="subtitle-2 text-muted text-capitalize">{{ weather.daily[5].weather[0].description }}</h6>
+                <h6 class="subtitle-1 fw-500">{{ weather.daily[5].dt | moment("dddd") }}</h6>
+                <div class="text-center">
+                  <h6 class="subtitle-1">{{ Math.round(weather.daily[5].temp.max) }}° - <span
+                    class="text-muted">{{ Math.round(weather.daily[5].temp.min) }}°</span></h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- 7 DAYS -->
+        <div class="wrapper">
+          <div class="card bg-frosted">
+            <div class="card-body">
+              <div class="text-center">
+                <IconWeather :weather="weather" number="6"></IconWeather>
+                <h6 class="subtitle-2 text-muted text-capitalize">{{ weather.daily[6].weather[0].description }}</h6>
+                <h6 class="subtitle-1 fw-500">{{ weather.daily[6].dt | moment("dddd") }}</h6>
+                <div class="text-center">
+                  <h6 class="subtitle-1">{{ Math.round(weather.daily[6].temp.max) }}° - <span
+                    class="text-muted">{{ Math.round(weather.daily[6].temp.min) }}°</span></h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import IconWeather from "@/components/weather/IconWeather";
+
+export default {
+  name: 'ForecastWeek',
+  props: [
+    'weather'
+  ],
+  components: {
+    IconWeather
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -120,142 +120,26 @@
       <OffcanvasUVI :weather="weather"></OffcanvasUVI>
 
       <!-- FORECAST 7 DAYS -->
-      <div class="row" v-if="weather">
-        <div class="col-md-12 col-lg-8">
-          <h6 class="subtitle-1 fw-500 text-white">This week</h6>
-        </div>
-        <div class="scrolling-wrapper">
-          <!-- TOMORROW - 1 DAY -->
-          <div class="wrapper">
-            <div class="card bg-frosted">
-              <div class="card-body">
-                <div class="text-center">
-                  <IconWeather :weather="weather" number="0"></IconWeather>
-                  <h6 class="subtitle-2 text-muted text-capitalize">{{ weather.daily[0].weather[0].description }}</h6>
-                  <h6 class="subtitle-1 fw-500">{{ new Date() | moment("add", "1 days", "dddd") }}</h6>
-                  <div class="text-center">
-                    <h6 class="subtitle-1">{{ Math.round(weather.daily[0].temp.max) }}° - <span
-                      class="text-muted">{{ Math.round(weather.daily[0].temp.min) }}°</span></h6>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- 2 DAYS -->
-          <div class="wrapper">
-            <div class="card bg-frosted">
-              <div class="card-body">
-                <div class="text-center">
-                  <IconWeather :weather="weather" number="1"></IconWeather>
-                  <h6 class="subtitle-2 text-muted text-capitalize">{{ weather.daily[1].weather[0].description }}</h6>
-                  <h6 class="subtitle-1 fw-500">{{ new Date() | moment("add", "2 days", "dddd") }}</h6>
-                  <div class="text-center">
-                    <h6 class="subtitle-1">{{ Math.round(weather.daily[1].temp.max) }}° - <span
-                      class="text-muted">{{ Math.round(weather.daily[1].temp.min) }}°</span></h6>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- 3 DAYS -->
-          <div class="wrapper">
-            <div class="card bg-frosted">
-              <div class="card-body">
-                <div class="text-center">
-                  <IconWeather :weather="weather" number="2"></IconWeather>
-                  <h6 class="subtitle-2 text-muted text-capitalize">{{ weather.daily[2].weather[0].description }}</h6>
-                  <h6 class="subtitle-1 fw-500">{{ new Date() | moment("add", "3 days", "dddd") }}</h6>
-                  <div class="text-center">
-                    <h6 class="subtitle-1">{{ Math.round(weather.daily[2].temp.max) }}° - <span
-                      class="text-muted">{{ Math.round(weather.daily[2].temp.min) }}°</span></h6>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- 4 DAYS -->
-          <div class="wrapper">
-            <div class="card bg-frosted">
-              <div class="card-body">
-                <div class="text-center">
-                  <IconWeather :weather="weather" number="3"></IconWeather>
-                  <h6 class="subtitle-2 text-muted text-capitalize">{{ weather.daily[3].weather[0].description }}</h6>
-                  <h6 class="subtitle-1 fw-500">{{ new Date() | moment("add", "4 days", "dddd") }}</h6>
-                  <div class="text-center">
-                    <h6 class="subtitle-1">{{ Math.round(weather.daily[3].temp.max) }}° - <span
-                      class="text-muted">{{ Math.round(weather.daily[3].temp.min) }}°</span></h6>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- 5 DAYS -->
-          <div class="wrapper">
-            <div class="card bg-frosted">
-              <div class="card-body">
-                <div class="text-center">
-                  <IconWeather :weather="weather" number="4"></IconWeather>
-                  <h6 class="subtitle-2 text-muted text-capitalize">{{ weather.daily[4].weather[0].description }}</h6>
-                  <h6 class="subtitle-1 fw-500">{{ new Date() | moment("add", "5 days", "dddd") }}</h6>
-                  <div class="text-center">
-                    <h6 class="subtitle-1">{{ Math.round(weather.daily[4].temp.max) }}° - <span
-                      class="text-muted">{{ Math.round(weather.daily[4].temp.min) }}°</span></h6>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- 6 DAYS -->
-          <div class="wrapper">
-            <div class="card bg-frosted">
-              <div class="card-body">
-                <div class="text-center">
-                  <IconWeather :weather="weather" number="5"></IconWeather>
-                  <h6 class="subtitle-2 text-muted text-capitalize">{{ weather.daily[5].weather[0].description }}</h6>
-                  <h6 class="subtitle-1 fw-500">{{ new Date() | moment("add", "6 days", "dddd") }}</h6>
-                  <div class="text-center">
-                    <h6 class="subtitle-1">{{ Math.round(weather.daily[5].temp.max) }}° - <span
-                      class="text-muted">{{ Math.round(weather.daily[5].temp.min) }}°</span></h6>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <!-- 7 DAYS -->
-          <div class="wrapper">
-            <div class="card bg-frosted">
-              <div class="card-body">
-                <div class="text-center">
-                  <IconWeather :weather="weather" number="6"></IconWeather>
-                  <h6 class="subtitle-2 text-muted text-capitalize">{{ weather.daily[6].weather[0].description }}</h6>
-                  <h6 class="subtitle-1 fw-500">{{ new Date() | moment("add", "7 days", "dddd") }}</h6>
-                  <div class="text-center">
-                    <h6 class="subtitle-1">{{ Math.round(weather.daily[6].temp.max) }}° - <span
-                      class="text-muted">{{ Math.round(weather.daily[6].temp.min) }}°</span></h6>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <ForecastWeek :weather="weather"></ForecastWeek>
 
     </div>
     <Footer footer-display="d-none d-md-block"></Footer>
   </div>
+
   <div v-else class="text-center mt-3">
     <LoadingState title="Loading..."></LoadingState>
   </div>
+
 </template>
 
 <script>
 // @ is an alias to /src
 import Footer from "@/components/includes/Footer";
-import IconWeather from "@/components/weather/IconWeather";
 import IconCurrentWeather from "@/components/weather/IconCurrentWeather";
 import ListUVI from "@/components/weather/ListUVI";
 import OffcanvasUVI from "@/components/weather/OffcanvasUVI";
 import LoadingState from "@/components/empty-states/LoadingState";
+import ForecastWeek from "@/components/weather/ForecastWeek";
 
 export default {
   name: 'Home',
@@ -263,11 +147,11 @@ export default {
     title: 'Home'
   },
   components: {
+    ForecastWeek,
     LoadingState,
     OffcanvasUVI,
     ListUVI,
     IconCurrentWeather,
-    IconWeather,
     Footer
   },
   data() {
