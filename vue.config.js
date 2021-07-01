b2f14e1f1e06c1f9696f69d23dc02a8a82794da8
Giancarlos Garza <giancarlosgza@gmail.com>
2021-07-01 16:02:43 -0500

    Vue config

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  pwa: {
+    name: "Weatherffy",
+    themeColor: "#1e8cd6",
+    msTileColor: "#000000",
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      name: "Weatherffy",
+      short_name: "Weatherffy",
+      background_color: "#ffffff",
+      theme_color: "#1e8cd6",
+    }
+  }
+};
